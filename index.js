cd7f057f8d5feee433a31e715334628aea6db957
Graeme Yeates <yeatesgraeme@gmail.com>
2015-08-07 17:19:49 -0400

    Initial WIP /cc @jdalton

---
+++ index.js
@@ -0,0 +1,46 @@
+var lodash = require('lodash');
+var vm = require('vm');
+var Promise = require('bluebird');
+var request = require('request-promise');
+var util = require('lodash-cli/lib/util');
+
+function makeSandbox() {
+  return vm.createContext({
+    require: function(file) {
+      console.log(file);
+      if (/lodash/.test(file)) return lodash;
+      else if (/util/.test(file)) return util;
+    },
+    module: {
+      exports: {}
+    }
+  });
+}
+
+request({
+  url: 'https://api.github.com/repos/lodash/lodash-cli/tags',
+  headers: {
+    'User-Agent': 'lodash-fetcher'
+  }
+}).then(function(tags) {
+  tags = JSON.parse(tags);
+
+  Promise.map(tags, function(tag) {
+    var url = 'https://raw.githubusercontent.com/lodash/lodash-cli/' + tag.name + '/lib/mapping.js';
+    return request.get(url)
+    .then(function(file) {
+      var sandbox = makeSandbox();
+      vm.runInContext(String(file), sandbox);
+      return sandbox.module.exports;
+    }, function() {
+      return null;
+    })
+  })
+  .then(function(modules) {
+    modules = lodash.filter(modules);
+
+    lodash.each(modules, function(module) {
+      console.log(lodash.keys(module));
+    });
+  });;
+});
